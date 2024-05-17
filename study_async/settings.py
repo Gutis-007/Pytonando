e58d7b3001280738c9d0a8693f10939d829eb240
Gutis007 <gutisilva123@hotmail.com>
2024-05-17 14:08:07 -0300

    Troca do banco de dados

--- study_async/settings.py
+++ study_async/settings.py
@@ -78,14 +78,14 @@ WSGI_APPLICATION = 'study_async.wsgi.application'
 
 DATABASES = {
     
-    'default': dj_database_url.parse(
-        'postgres://pytonando_user:CnPf9e72Oiyqv93nyqD4N85Jj9YJqCPS@dpg-cp2m4763e1ms73f1u87g-a.oregon-postgres.render.com/pytonando',
-    )
+    #'default': dj_database_url.parse(
+    #    'postgres://pytonando_user:CnPf9e72Oiyqv93nyqD4N85Jj9YJqCPS@dpg-cp2m4763e1ms73f1u87g-a.oregon-postgres.render.com/pytonando',
+    #)
     
- #'default': {
-  #     'ENGINE': 'django.db.backends.sqlite3',
-  #      'NAME': BASE_DIR / 'db.sqlite3',
-  #  }
+ 'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
@@ -122,9 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static'),]
+STATIC_ROOT = os.path.join('static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
